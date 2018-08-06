71968744c649c35d4bfab6c35be62e58508d59e8
Thomas Krause <thomaskrause@posteo.de>
2018-08-06 19:31:41 +0200

    Add script to either download or compile graphANNIS C-API

---
+++ misc/get-graphannis.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+if [ -z "$GRAPHANNIS_VERSION" ]; then
+  # travis not set, but TRAVIS_TAG might be
+  if [ -z "$TRAVIS_TAG" ]; then
+    # also not set
+    echo "Not a Travis deploy build"
+  else
+    # TRAVIS_TAG is set, set GRAPHANNIS_VERSION to its value
+    GRAPHANNIS_VERSION=$TRAVIS_TAG
+  fi
+fi
+
+if [ -z "$GRAPHANNIS_VERSION" ]; then
+  # compile latest development
+  mkdir "$DIR"/../ext/
+  git clone https://github.com/corpus-tools/graphANNIS "$DIR"/../ext/graphANNIS
+  cd "$DIR"/../ext/graphANNIS
+  cargo build --release
+  cp target/release/libgraphannis_capi.so "$DIR"/../java-api/src/main/resources/linux-x86-64/
+  cd "$DIR"/..
+else
+  # get released version
+  echo "getting $GRAPHANNIS_VERSION"
+  "$DIR"/download-release-binaries.sh "$GRAPHANNIS_VERSION"
+fi
