f04b85f95187f10c7e5f240c7d3cbe2f4ddaf1b4
Thomas <thomaskrause@posteo.de>
2019-01-16 15:31:00 +0100

    Use the enum only

--- src/main/java/org/corpus_tools/graphannis/model/Component.java
+++ src/main/java/org/corpus_tools/graphannis/model/Component.java
@@ -1,42 +1,16 @@
 package org.corpus_tools.graphannis.model;
 
-import org.corpus_tools.graphannis.capi.AnnisComponentType;
-
 public class Component {
 
-	public enum Type {
-		Coverage, Dominance, Pointing, Ordering, LeftToken, RightToken, PartOfSubcorpus;
-		
-		public static Type fromInt(int ctype) {
-			switch (ctype) {
-			case AnnisComponentType.Coverage:
-				return Type.Coverage;
-			case AnnisComponentType.Dominance:
-				return Type.Dominance;
-			case AnnisComponentType.Pointing:
-				return Type.Pointing;
-			case AnnisComponentType.Ordering:
-				return Type.Ordering;
-			case AnnisComponentType.LeftToken:
-				return Type.LeftToken;
-			case AnnisComponentType.RightToken:
-				return Type.RightToken;
-			case AnnisComponentType.PartOfSubcorpus:
-				return Type.PartOfSubcorpus;
-			}
-			throw new IllegalArgumentException("Invalid component type " + ctype);
-		}
-	}
-
-	private Type ctype;
+	private ComponentType ctype;
 	private String name;
 	private String layer;
 
-	public Type getType() {
+	public ComponentType getType() {
 		return ctype;
 	}
 
-	public void setType(Type ctype) {
+	public void setType(ComponentType ctype) {
 		this.ctype = ctype;
 	}
 
