dd3082c3bca620c188dae8856f9841e9ce357d1f
Thomas <thomaskrause@posteo.de>
2019-03-05 16:19:29 +0100

    Add javadoc to utililty function

--- src/main/java/org/corpus_tools/graphannis/Util.java
+++ src/main/java/org/corpus_tools/graphannis/Util.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Util {
+    /** Takes a match identifier (which includes the matched annotation name) and returns the node name. */
     public static List<String> nodeNamesFromMatch(String matchLine) {
         List<String> result = new ArrayList<>();
         if(matchLine != null) {
