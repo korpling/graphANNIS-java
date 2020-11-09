b5ab4491a061b5a3ca1c6727a0cef4b1ba45f0f7
Thomas Krause <krauseto@hu-berlin.de>
2020-11-09 14:01:31 +0100

    Add deploy workflow to deploy as P2 repository

--- misc/deploy-p2.sh
+++ misc/deploy-p2.sh
@@ -3,22 +3,22 @@
 # Used by CI to deploy the existing target/repository/<version> directory to Github Pages
 
 if [ -n "$GITHUB_API_KEY" ]; then
-    cd "$TRAVIS_BUILD_DIR"
+    cd "$GITHUB_WORKSPACE"
 
-    echo "cloning gh-pages from ${TRAVIS_REPO_SLUG}"
-    git clone -q  -b gh-pages https://$GITHUB_API_KEY@github.com/${TRAVIS_REPO_SLUG} gh-pages &>/dev/null
+    echo "cloning gh-pages from ${GITHUB_REPOSITORY}"
+    git clone -q  -b gh-pages https://$GITHUB_API_KEY@github.com/${GITHUB_REPOSITORY} gh-pages &>/dev/null
     cd gh-pages
     mkdir -p p2/${SHORT_VERSION}
     cd p2/${SHORT_VERSION}
     # remove all old file
     rm -Rf *
     # copy the P2 repository content from the maven build directory
-    cp -R ${TRAVIS_BUILD_DIR}/target/repository/* .
+    cp -R ${GITHUB_WORKSPACE}/target/repository/* .
     git add .
     git -c user.name='travis' -c user.email='travis' commit -m "add p2 repository for version ${SHORT_VERSION}"
-    echo "pushing to gh-pages to ${TRAVIS_REPO_SLUG}"
-    git push -q https://$GITHUB_API_KEY@github.com/${TRAVIS_REPO_SLUG} gh-pages &>/dev/null
-    cd "$TRAVIS_BUILD_DIR"
+    echo "pushing to gh-pages to ${GITHUB_REPOSITORY}"
+    git push -q https://$GITHUB_API_KEY@github.com/${GITHUB_REPOSITORY} gh-pages &>/dev/null
+    cd "$GITHUB_WORKSPACE"
 else
 	>&2 echo "Cannot deploy P2 repository because GITHUB_API_KEY environment variable is not set"
 	exit 1
